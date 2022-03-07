a3e9fb4a8e2ab871f5d20498c2aeca5f5c27f72a
AladdinRukhiddinov <aladinrukhiddinov@gmail.com>
2022-03-07 18:43:01 +0500

    create new Project studentCRUD

---
+++ src/main/java/uz/pdp/studencrud/StudenCrudApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.studencrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudenCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/studencrud/StudenCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.studencrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
